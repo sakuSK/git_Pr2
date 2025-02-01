f6ca6f964d3eb644b1baac9b4ead2e51d0820a90
sakuSK <sakutakonno@gmail.com>
2025-02-01 17:28:34 +0900

    BMIアプリ新機能実装

--- No2.py
+++ No2.py
@@ -4,6 +4,7 @@ import streamlit as st
 st.session_state.bmi = 0
 st.session_state.h = 0
 st.session_state.w = 0
+st.session_state.best_w = 0
 if 'resalt' not in st.session_state:
     st.session_state.resalt = 0 
 def calculate_bmi(height, weight):
@@ -11,11 +12,17 @@ def calculate_bmi(height, weight):
     st.session_state.resalt = f"{st.session_state.bmi:.2f}"  # f-stringを使ってフォーマットするよ！
     st.write(f"あなたのBMIは{st.session_state.resalt}です")
 
+def best_weight(height, weight):
+    st.session_state.best_w = (height** 2) *2   # 適正体重 ＝ (身長m)2 ×22
+    st.session_state.resalt_w = f"{st.session_state.best_w:.2f}"  # f-stringを使ってフォーマットするよ！
+    st.write(f"あなたの身長の適正体重は{st.session_state.best_w}です")
+
 st.session_state.h = st.slider("身長を入力してください", 0, 300, 160)
 st.session_state.w = st.slider("体重を入力してください", 0, 300, 60)
 
 # スライダーの後にBMIを計算する
 calculate_bmi(st.session_state.h, st.session_state.w)
+best_weight(st.session_state.h, st.session_state.w)
 def clear_file(file_path):
     try:
         with open(file_path, "w") as file:
